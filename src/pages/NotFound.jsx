f4fc77c8f67d6e02a9f93d93a979b4693a41617f
sultan <sultanhasanov987@gmail.com>
2022-12-08 23:10:04 +0300

    добавил страницу notFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            
-        </div>
+      <div className="error">
+        <h1>
+          Ничего не найдено <span>😕</span>
+        </h1>
+        <h1>
+          Перейдите на главную
+          <Link to="/">
+            <span className="error_pages">страницу</span>
+          </Link>
+        </h1>
+      </div>
     );
 };
 
